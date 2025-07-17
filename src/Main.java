7745c90d83566de67afebf3d3f319e4f9b0c31f7
Fatima <fat.tka4enko2014@yandex.ru>
2025-07-17 14:32:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 735; //стоимость билета
+        int miles = 20;
+
+        System.out.println(ticketPrice / miles);
+    }
+}
